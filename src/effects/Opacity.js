147ba9ff3f5c8380b1351420a24d8de374057723
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-12 15:57:11 -0400

    Code view fix

--- src/effects/Opacity.js
+++ src/effects/Opacity.js
@@ -22,19 +22,44 @@ const opacity = () => {
         <span className="hl-attribute">{`button `}</span>
         <span className="hl-bracket">{`{`}</span> <br />
         <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> color:</span>
+          <span className="hl hl-value"> #1D9AF2;</span>
+          <br />
+          <span className="hl hl-property"> background-color:</span>
+          <span className="hl hl-value"> #292D3E;</span>
+          <br />
+          <span className="hl hl-property">border: </span>
+          <span className="hl hl-value"> 1px solid #1D9AF2;</span>
+          <br />
+          <span className="hl hl-property"> border-radius:</span>
+          <span className="hl hl-value"> 4px;</span>
+          <br />
+          <span className="hl hl-property">padding: </span>
+          <span className="hl hl-value"> 0 15px;</span>
+          <br />
+          <span className="hl hl-property">cursor:</span>
+          <span className="hl hl-value"> pointer;</span>
+          <br />
+          <span className="hl hl-property">height:</span>
+          <span className="hl hl-value">32px;</span>
+          <br />
+          <span className="hl hl-property">font-size:</span>
+          <span className="hl hl-value">14px;</span>
+          <br />
           <span className="hl hl-property"> transition:</span>
           <span className="hl hl-value"> all 0.2s ease-in-out;</span>
           <br />
           <span className="hl hl-property"> opacity:</span>
           <span className="hl hl-value"> 0.5;</span>
           <br />
-          <span className="hl hl-property">{`&:hover`} </span>
-          <span style={{ color: "#ae81ff" }}>{`{`}</span> <br />
-          <span className="hl hl-property"> {`opacity:`}</span>
+        </div>
+        <span className="hl-bracket">{`}`}</span>
+        <br />
+        <span className="hl-attribute">{`button:hover `}</span>
+        <span className="hl-bracket">{`{`}</span> <br />
+        <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> opacity:</span>
           <span className="hl hl-value"> 1;</span>
-          <br />
-          <span style={{ color: "#ae81ff" }}>{`}`}</span>
-          <br />
         </div>
         <span className="hl-bracket">{`}`}</span>
       </div>
@@ -54,11 +79,20 @@ const opacityHtmlVariable = `<button>Opacity</button>`;
 
 const opacityCssVariable = `
  button {
+  color: #1D9AF2;
+  background-color: #292D3E;
+  border: 1px solid #1D9AF2;
+  border-radius: 4px;
+  padding: 0 15px;
+  cursor: pointer;
+  height: 32px;
+  font-size: 14px;
   transition: all 0.2s ease-in-out;
   opacity: 0.5;
-  &:hover {
-    opacity: 1;
-  }
-}`;
+}
+button:hover{
+  opacity: 1;
+}
+`;
 
 export { opacity, opacityStyle, opacityHtmlVariable, opacityCssVariable };
