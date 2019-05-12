d847086fc56d947506ad68525d046665c310a388
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-12 16:57:52 -0400

    Twitter button implement

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,9 @@ import { css } from "emotion";
 
 import "antd/dist/antd.css";
 import cssWand from "../../css-wand.png";
+
+import { TwitterShareButton } from "react-twitter-embed";
+
 const Header = () => {
   return (
     <div>
@@ -18,20 +21,32 @@ const Header = () => {
           <img src={cssWand} height="100" alt="logo" />
         </Col>
         <Col xs={24} md={12}>
-          <a
-            href="https://github.com/ant-design/ant-design"
-            className={css`
-              color: #fff;
-              opacity: 0.8;
-              transition: all 0.2s ease-in-out !important;
-              &:hover {
-                opacity: 1;
-                color: #fff;
-              }
-            `}
-          >
-            <Icon type="github" style={{ fontSize: "28px" }} />
-          </a>
+          <Row justify="start" type="flex" align="middle">
+            <Col md={10} />
+            <Col md={2}>
+              <a
+                href="https://github.com/ant-design/ant-design"
+                className={css`
+                  color: #fff;
+                  opacity: 0.8;
+                  transition: all 0.2s ease-in-out !important;
+                  &:hover {
+                    opacity: 1;
+                    color: #fff;
+                  }
+                `}
+              >
+                <Icon type="github" style={{ fontSize: "28px" }} />
+              </a>
+            </Col>
+            <Col md={2} align="left">
+              <TwitterShareButton
+                url={"https://github.com/oliver-gomes"}
+                options={{ text: "#reactjs is awesome" }}
+              />
+            </Col>
+            <Col md={10} />
+          </Row>
         </Col>
       </Row>
     </div>
