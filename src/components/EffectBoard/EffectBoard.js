38ae70d7202a112dc749702e5cdc777d62dd864b
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-12 13:44:09 -0400

    2nd Row implement

--- src/components/EffectBoard/EffectBoard.js
+++ src/components/EffectBoard/EffectBoard.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+
 import { Row, Col } from "antd";
 import "antd/dist/antd.css";
+
 export default class EffectBoard extends Component {
   render() {
     return (
       <>
-        <Row>
+        {/* Row 1 */}
+        <Row type="flex" align="middle">
+          <Col xs={0} sm={0} md={6} />
+          <Col xs={24} sm={24} md={2} className="column-space">
+            asdf
+          </Col>
+          <Col xs={24} sm={24} md={2} className="column-space">
+            asdf
+          </Col>
+          <Col xs={24} sm={24} md={2} className="column-space">
+            asdf
+          </Col>
+          <Col xs={24} sm={24} md={2} className="column-space">
+            asdf
+          </Col>
+          <Col xs={24} sm={24} md={2} className="column-space">
+            asdf
+          </Col>
+          <Col xs={24} sm={24} md={2} className="column-space">
+            asdf
+          </Col>
+          <Col xs={0} sm={0} md={6} />
+        </Row>
+
+        {/* Row 2 */}
+        <Row style={{ marginTop: "50px", marginBottom: "20px" }}>
           <Col xs={0} sm={0} md={6} />
           <Col xs={24} sm={24} md={2} className="column-space">
             asdf
