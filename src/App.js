46226bcb8f5df4acb52e28a6bd68ea091631fac8
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-12 13:42:19 -0400

    EffectBoard import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import EffectBoard from "./components/EffectBoard/EffectBoard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <EffectBoard />
     </div>
   );
 }
