ed3159abc3a556095b49e786aee97852e4f70bf6
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-13 10:16:19 -0400

    Style implementation

--- src/effects/Swing.js
+++ src/effects/Swing.js
@@ -4,16 +4,73 @@ import { css, cx } from "emotion";
 const swing = () => {
   return (
     <div className="hl">
-      <h5 style={{ color: "#fff" }}>HTML </h5>
-      <p>
-        <span className="hl-attribute">swing</span>
-      </p>
-
-      <span className="hl hl-attribute"> transition:</span>
-      <span className="hl hl-number"> all 0.2s ease-in-out;</span>
+      <p style={{ color: "#fff", fontSize: "26px;" }}>HTML </p>
+      <div style={{ backgroundColor: "#262A39" }}>
+        <span className="hl-bracket">{`<`}</span>
+        <span className="hl-attribute">{`button`}</span>
+        <span className="hl-bracket">{`>`}</span>
+        <span>{`Square to Circle`}</span>
+        <span className="hl-bracket">{`<`}</span>
+        <span className="hl-bracket">{`/`}</span>
+        <span className="hl-attribute">{`button `}</span>
+        <span className="hl-bracket">{`>`}</span>
+      </div>
       <br />
-      <span className="hl hl-attribute">&:hover </span>
-      <span className="hl hl-number"> transform: scale(1.1);</span>
+      <br />
+      <p style={{ color: "#fff", fontSize: "26px;" }}>CSS</p>
+      <div style={{ backgroundColor: "#262A39" }}>
+        <span className="hl-attribute">{`button `}</span>
+        <span className="hl-bracket">{`{`}</span> <br />
+        <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> color:</span>
+          <span className="hl hl-value"> #1D9AF2;</span>
+          <br />
+          <span className="hl hl-property"> background-color:</span>
+          <span className="hl hl-value"> #292D3E;</span>
+          <br />
+          <span className="hl hl-property">border: </span>
+          <span className="hl hl-value"> 1px solid #1D9AF2;</span>
+          <br />
+          <span className="hl hl-property"> border-radius:</span>
+          <span className="hl hl-value"> 4px;</span>
+          <br />
+          <span className="hl hl-property">padding: </span>
+          <span className="hl hl-value"> 0 15px;</span>
+          <br />
+          <span className="hl hl-property">cursor:</span>
+          <span className="hl hl-value"> pointer;</span>
+          <br />
+          <span className="hl hl-property">height:</span>
+          <span className="hl hl-value">32px;</span>
+          <br />
+          <span className="hl hl-property">font-size:</span>
+          <span className="hl hl-value">14px;</span>
+          <br />
+          <span className="hl hl-property"> transition:</span>
+          <span className="hl hl-value"> all 0.2s ease-in-out;</span>
+          <br />
+        </div>
+        <span className="hl-bracket">{`}`}</span>
+        <br />
+        <span className="hl-attribute">{`button:hover `}</span>
+        <span className="hl-bracket">{`{`}</span> <br />
+        <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> animation: </span>
+          <span className="hl hl-value"> swing 1s ease;</span>
+          <br />
+          <span className="hl hl-property"> animation-iteration-count:</span>
+          <span className="hl hl-value"> 1;</span>
+        </div>
+        <span className="hl-bracket">{`}`}</span>
+        <br />
+        <span className="hl-attribute">{`@keyframes swing `}</span>
+        <span className="hl-bracket">{`{`}</span> <br />
+        <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> 15% </span>
+          <span className="hl-bracket">{`{`}</span> <br />
+          <span className="hl hl-value"> transform: translateX(5px);</span>
+        </div>
+      </div>
     </div>
   );
 };
