30ab7a48b5c0439e94eadf259f56986c9d8e3057
Oliver Gomes <ogomesphoto@gmail.com>
2019-05-14 12:18:35 -0400

    Spinner 2 add

---
+++ src/effects/Spinner2.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { css, cx } from "emotion";
+
+const spinner2 = () => {
+  return (
+    <div className="hl">
+      <p style={{ color: "#fff", fontSize: "26px;" }}>HTML </p>
+      <div style={{ backgroundColor: "#262A39" }}>
+        <span className="hl-bracket">{`<`}</span>
+        <span className="hl-attribute">{`button`}</span>
+        <span className="hl-bracket">{`>`}</span>
+        <span>{`spinner`}</span>
+        <span className="hl-bracket">{`<`}</span>
+        <span className="hl-bracket">{`/`}</span>
+        <span className="hl-attribute">{`button `}</span>
+        <span className="hl-bracket">{`>`}</span>
+      </div>
+      <br />
+      <br />
+      <p style={{ color: "#fff", fontSize: "26px;" }}>CSS</p>
+      <div style={{ backgroundColor: "#262A39" }}>
+        <span className="hl-attribute">{`button `}</span>
+        <span className="hl-bracket">{`{`}</span> <br />
+        <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> color:</span>
+          <span className="hl hl-value"> #1D9AF2;</span>
+          <br />
+          <span className="hl hl-property"> background-color:</span>
+          <span className="hl hl-value"> #292D3E;</span>
+          <br />
+          <span className="hl hl-property">border: </span>
+          <span className="hl hl-value"> 1px solid #1D9AF2;</span>
+          <br />
+          <span className="hl hl-property"> border-radius:</span>
+          <span className="hl hl-value"> 4px;</span>
+          <br />
+          <span className="hl hl-property">padding: </span>
+          <span className="hl hl-value"> 0 15px;</span>
+          <br />
+          <span className="hl hl-property">cursor:</span>
+          <span className="hl hl-value"> pointer;</span>
+          <br />
+          <span className="hl hl-property">height:</span>
+          <span className="hl hl-value">32px;</span>
+          <br />
+          <span className="hl hl-property">font-size:</span>
+          <span className="hl hl-value">14px;</span>
+          <br />
+          <span className="hl hl-property"> transition:</span>
+          <span className="hl hl-value"> all 0.2s ease-in-out;</span>
+          <br />
+          <span className="hl hl-property"> spinner:</span>
+          <span className="hl hl-value"> 0.5;</span>
+          <br />
+        </div>
+        <span className="hl-bracket">{`}`}</span>
+        <br />
+        <span className="hl-attribute">{`button:hover `}</span>
+        <span className="hl-bracket">{`{`}</span> <br />
+        <div style={{ marginLeft: "20px" }}>
+          <span className="hl hl-property"> spinner:</span>
+          <span className="hl hl-value"> 1;</span>
+        </div>
+        <span className="hl-bracket">{`}`}</span>
+      </div>
+    </div>
+  );
+};
+
+const spinner2Style = css`
+  height: 40px;
+  width: 40px;
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  border: 2px solid rgba(0, 0, 0, 0.4);
+  border-top-color: #fff;
+  animation: spinner2 600ms linear infinite;
+}
+@keyframes spinner2 {
+  to {
+      transform: rotate(360deg);
+  }
+}
+`;
+
+export { spinner2, spinner2Style };
